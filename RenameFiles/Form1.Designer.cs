814791232f281cae3c96ab26aed41894bc4ba68d
abdessalam-aadel <abdessalam.aadel@gmail.com>
2022-10-09 19:55:19 +0100

    New Update

--- RenameFiles/Form1.Designer.cs
+++ RenameFiles/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             this.btnLoad = new System.Windows.Forms.Button();
             this.btnRename = new System.Windows.Forms.Button();
             this.labelTextTotales = new System.Windows.Forms.Label();
@@ -35,16 +36,24 @@
             this.labelGit = new System.Windows.Forms.Label();
             this.txtBoxLoad = new System.Windows.Forms.TextBox();
             this.txtDone = new System.Windows.Forms.Label();
+            this.picArrowDown = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picCheck = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picArrowDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCheck)).BeginInit();
             this.SuspendLayout();
             // 
             // btnLoad
             // 
             this.btnLoad.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLoad.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.ForeColor = System.Drawing.Color.Teal;
             this.btnLoad.Location = new System.Drawing.Point(306, 12);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(0);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.Size = new System.Drawing.Size(75, 20);
             this.btnLoad.TabIndex = 0;
             this.btnLoad.Text = "Load ...";
             this.btnLoad.UseVisualStyleBackColor = true;
@@ -52,21 +61,24 @@
             // 
             // btnRename
             // 
+            this.btnRename.BackColor = System.Drawing.Color.Tomato;
             this.btnRename.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRename.FlatAppearance.BorderSize = 0;
             this.btnRename.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRename.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnRename.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRename.ForeColor = System.Drawing.Color.Moccasin;
             this.btnRename.Location = new System.Drawing.Point(12, 38);
             this.btnRename.Name = "btnRename";
             this.btnRename.Size = new System.Drawing.Size(288, 23);
             this.btnRename.TabIndex = 1;
             this.btnRename.Text = "Rename";
-            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.UseVisualStyleBackColor = false;
             this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
             // 
             // labelTextTotales
             // 
             this.labelTextTotales.AutoSize = true;
-            this.labelTextTotales.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.labelTextTotales.ForeColor = System.Drawing.SystemColors.InfoText;
             this.labelTextTotales.Location = new System.Drawing.Point(124, 64);
             this.labelTextTotales.Name = "labelTextTotales";
             this.labelTextTotales.Size = new System.Drawing.Size(72, 13);
@@ -76,7 +88,7 @@
             // txtTotales
             // 
             this.txtTotales.AutoSize = true;
-            this.txtTotales.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtTotales.ForeColor = System.Drawing.SystemColors.GrayText;
             this.txtTotales.Location = new System.Drawing.Point(196, 64);
             this.txtTotales.Name = "txtTotales";
             this.txtTotales.Size = new System.Drawing.Size(16, 13);
@@ -88,8 +100,8 @@
             this.labelGit.AutoSize = true;
             this.labelGit.Cursor = System.Windows.Forms.Cursors.Hand;
             this.labelGit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelGit.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.labelGit.Location = new System.Drawing.Point(177, 86);
+            this.labelGit.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.labelGit.Location = new System.Drawing.Point(173, 87);
             this.labelGit.Name = "labelGit";
             this.labelGit.Size = new System.Drawing.Size(38, 13);
             this.labelGit.TabIndex = 4;
@@ -104,16 +116,53 @@
             this.txtBoxLoad.Name = "txtBoxLoad";
             this.txtBoxLoad.Size = new System.Drawing.Size(288, 20);
             this.txtBoxLoad.TabIndex = 5;
-            this.txtBoxLoad.Text = "Chose your folde ....";
+            this.txtBoxLoad.Text = "Chose your folder ....";
             // 
             // txtDone
             // 
             this.txtDone.AutoSize = true;
+            this.txtDone.BackColor = System.Drawing.Color.Transparent;
+            this.txtDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtDone.ForeColor = System.Drawing.Color.Maroon;
-            this.txtDone.Location = new System.Drawing.Point(345, 64);
+            this.txtDone.Location = new System.Drawing.Point(320, 64);
             this.txtDone.Name = "txtDone";
             this.txtDone.Size = new System.Drawing.Size(0, 13);
             this.txtDone.TabIndex = 6;
+            // 
+            // picArrowDown
+            // 
+            this.picArrowDown.BackColor = System.Drawing.Color.Transparent;
+            this.picArrowDown.Image = global::RenameFiles.Properties.Resources.Arrows_down_animated;
+            this.picArrowDown.Location = new System.Drawing.Point(313, 43);
+            this.picArrowDown.Name = "picArrowDown";
+            this.picArrowDown.Size = new System.Drawing.Size(62, 50);
+            this.picArrowDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picArrowDown.TabIndex = 7;
+            this.picArrowDown.TabStop = false;
+            this.picArrowDown.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Tomato;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(262, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 23);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picCheck
+            // 
+            this.picCheck.BackColor = System.Drawing.Color.Transparent;
+            this.picCheck.Image = ((System.Drawing.Image)(resources.GetObject("picCheck.Image")));
+            this.picCheck.Location = new System.Drawing.Point(362, 60);
+            this.picCheck.Name = "picCheck";
+            this.picCheck.Size = new System.Drawing.Size(19, 20);
+            this.picCheck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCheck.TabIndex = 9;
+            this.picCheck.TabStop = false;
+            this.picCheck.Visible = false;
             // 
             // FrmMain
             // 
@@ -122,7 +171,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(388, 103);
+            this.Controls.Add(this.picCheck);
             this.Controls.Add(this.txtDone);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picArrowDown);
             this.Controls.Add(this.txtBoxLoad);
             this.Controls.Add(this.labelGit);
             this.Controls.Add(this.txtTotales);
@@ -131,12 +183,16 @@
             this.Controls.Add(this.btnLoad);
             this.ForeColor = System.Drawing.SystemColors.WindowText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmMain";
-            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Rename Multiple Files (JPG) in Multiples Folder";
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FrmMain_DragDrop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FrmMain_DragEnter);
+            this.DragLeave += new System.EventHandler(this.FrmMain_DragLeave);
+            ((System.ComponentModel.ISupportInitialize)(this.picArrowDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCheck)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,6 +207,9 @@
         private System.Windows.Forms.Label labelGit;
         private System.Windows.Forms.TextBox txtBoxLoad;
         private System.Windows.Forms.Label txtDone;
+        private System.Windows.Forms.PictureBox picArrowDown;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picCheck;
     }
 }
 
--- RenameFiles/Form1.cs
+++ RenameFiles/Form1.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace RenameFiles
 {
     public partial class FrmMain : Form
     {
+        // Array of JPG Files found in Folder
         string[] JPGfiles;
+        // Creat variable to Counting the number of files in a folder 
         int fileCount = 0;
 
         public FrmMain()
@@ -15,13 +19,15 @@ namespace RenameFiles
             InitializeComponent();
         }
 
+        // Handle Methode Search Directory and Get all JPG files found,
+        // and bring out to Array of string
         public static int SearchDirectoryTree(string path, out string[] JPGfiles)
         {
             JPGfiles = Directory.GetFiles(path, "*.jpg", SearchOption.AllDirectories);
             return JPGfiles.Length;
         }
 
-
+        // Handle Event Click of Buttton Load Folder
         private void btnLoad_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog FD = new FolderBrowserDialog();
@@ -33,43 +39,110 @@ namespace RenameFiles
             }
         }
 
+        // Handle Event Click of Buttton Rename
         private void btnRename_Click(object sender, EventArgs e)
         {
-            foreach (string filePath in JPGfiles)
+            try
             {
                 Cursor = Cursors.WaitCursor;
-                FileInfo finfo = new FileInfo(filePath);
-                string nameFile = Path.GetFileNameWithoutExtension(finfo.Name);
-                int nbr = Convert.ToInt32(nameFile);
-                if (nbr != 1)
+
+                // Creat list of all names in jpg files array
+                List<int> listNbr = new List<int>();
+                
+                foreach (string filePath in JPGfiles)
                 {
-                    nbr -= 1;
-                    string newName = finfo.DirectoryName + @"\" + nbr + ".jpg";
-                    File.Move(filePath, newName);
+                    FileInfo finfo = new FileInfo(filePath);
+                    string nameFile = Path.GetFileNameWithoutExtension(finfo.Name);
+                    // Convert string to integer
+                    int nbr = Convert.ToInt32(nameFile);
+                    // Add name to the list
+                    listNbr.Add(nbr);
                 }
 
+                for (int i = 1; i <= listNbr.Max() - 3; i++)
+                {
+                    foreach (string filePath in JPGfiles)
+                    {
+                        FileInfo finfo = new FileInfo(filePath);
+                        string nameFile = Path.GetFileNameWithoutExtension(finfo.Name);
+                        int nbr = Convert.ToInt32(nameFile);
+                        if (nbr > 2)
+                        {
+                            nbr -= 1;
+                            // Creat the same path and new name for the jpg file
+                            string newName = finfo.DirectoryName + @"\" + nbr + ".jpg";
+
+                            try
+                            {
+                                // Move the file
+                                File.Move(filePath, newName);
+                            }
+
+                            // use IOException to check if the new path is already exists.
+                            catch (IOException)
+                            {
+                                // using continue statement in for each loop 
+                                // to skip the further execution of the statements
+                                // and transfer the controls back to the 
+                                // next iteration of the for each loop 
+                                continue;
+                            }
+
+                            // Replace old path with new path in JPGfiles Array
+                            JPGfiles[Array.IndexOf(JPGfiles, filePath)] = newName;
+                        }
+                    }
+                }
+
+                Cursor = Cursors.Default;
+                picCheck.Visible = true;
+                txtDone.Text = "Done!";
             }
 
-            Cursor = Cursors.Default;
-            txtDone.Text = "Done!";
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Cursor = Cursors.Default;
+                txtDone.Text = "";
+                picCheck.Visible = false;
+                txtBoxLoad.Text = "Chose your folder ....";
+                txtTotales.Text = "...";
+            }
         }
 
         private void labelGit_Click(object sender, EventArgs e)
         {
+            // Go to Github
             Process.Start("https://github.com/abdessalam-aadel");
         }
 
         private void FrmMain_DragDrop(object sender, DragEventArgs e)
         {
             string path = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
-            txtBoxLoad.Text = path;
-            fileCount = SearchDirectoryTree(path, out JPGfiles);
-            txtTotales.Text = fileCount + " files.";
+            if (Directory.Exists(path))
+            {
+                txtBoxLoad.Text = path;
+                fileCount = SearchDirectoryTree(path, out JPGfiles);
+                txtTotales.Text = fileCount + " files.";
+                picArrowDown.Visible = false;
+            }
+            else
+                picArrowDown.Visible = false;
         }
 
         private void FrmMain_DragEnter(object sender, DragEventArgs e)
         {
             e.Effect = DragDropEffects.Copy;
+            txtBoxLoad.Text = "Chose your folder ....";
+            txtTotales.Text = "...";
+            picArrowDown.Visible = true;
+            txtDone.Text = "";
+            picCheck.Visible = false;
+        }
+
+        private void FrmMain_DragLeave(object sender, EventArgs e)
+        {
+            picArrowDown.Visible = false;
         }
     }
 }
